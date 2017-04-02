7538b3446b45be8d9de0c574baf29da32833bce2
halldorajona <halldorajona@gmail.com>
2017-04-02 21:03:33 +0000

    Revert "AddFriendsToGroup checkList :fried_shrimp:"
    This reverts commit 0dc38760936d31d14b0d01c5ac3c28d35fef3b2c.

--- app/src/main/java/com/example/svava/planguin/Activities/GroupPageActivity.java
+++ app/src/main/java/com/example/svava/planguin/Activities/GroupPageActivity.java
@@ -43,9 +43,6 @@ public class GroupPageActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, groupFriends);
 
-        Bundle b = getIntent().getExtras();
-        String[] resultAtt = b.getStringArray("selectedItems");
-
         TextView textView = (TextView) findViewById(R.id.GroupName);
         textView.setText(currentGroup);
 
@@ -114,7 +111,7 @@ public class GroupPageActivity extends AppCompatActivity {
                 overridePendingTransition(0, 0);
                 break;
             case R.id.addfriendstogroup_button:
-                i = new Intent(this, AddFriendsToGroupActivity.class);
+                i = new Intent(this, FriendListActivity.class);
                 startActivity(i);
                 overridePendingTransition(0, 0);
                 break;
