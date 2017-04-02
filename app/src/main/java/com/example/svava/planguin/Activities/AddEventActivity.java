9a39f79f5116a265d23f085b61a71248b245a05b
thorunn <thorunnarna93@gmail.com>
2017-04-02 18:21:54 +0000

    nýtt activity og smá fikt

---
+++ app/src/main/java/com/example/svava/planguin/Activities/AddEventActivity.java
@@ -0,0 +1,54 @@
+package com.example.svava.planguin.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.svava.planguin.R;
+
+public class AddEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+
+    public void onClick(View v) {
+
+        Intent i;
+        int id = v.getId();
+
+        switch(id){
+            case R.id.compare_button:
+                i = new Intent(this, CompareActivity.class);
+                startActivity(i);
+                overridePendingTransition(0, 0);
+                break;
+            case R.id.friendlist_button:
+                i = new Intent(this, FriendListActivity.class);
+                startActivity(i);
+                overridePendingTransition(0, 0);
+                break;
+            case R.id.grouplist_button:
+                i = new Intent(this, GroupListActivity.class);
+                startActivity(i);
+                overridePendingTransition(0, 0);
+                break;
+            case R.id.settings_button:
+                i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                overridePendingTransition(0, 0);
+                break;
+            case R.id.invitation_button:
+                i = new Intent(this, InvitationActivity.class);
+                startActivity(i);
+                overridePendingTransition(0, 0);
+                break;
+            default:
+                i = new Intent(this, ScheduleActivity.class);
+                startActivity(i);
+                break;
+        }
+}
