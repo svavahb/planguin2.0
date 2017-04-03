215577fb55b4d81cf544b8244a64d1bb600474de
thorunn <thorunnarna93@gmail.com>
2017-04-03 11:12:47 +0000

    vinna í add event :calendar: :cactus:

--- app/src/main/java/com/example/svava/planguin/Activities/AddEventActivity.java
+++ app/src/main/java/com/example/svava/planguin/Activities/AddEventActivity.java
@@ -4,15 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import com.example.svava.planguin.R;
 
 public class AddEventActivity extends AppCompatActivity {
 
+    private TimePicker simpleTimePicker_Start;
+    private TimePicker simpleTimePicker_End;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+        DatePicker simpleDatePicker_Start = (DatePicker)findViewById(R.id.simpleDatePicker_Start); // initiate a date picker
+        simpleDatePicker_Start.setSpinnersShown(false);
+
+        DatePicker simpleDatePicker_End = (DatePicker)findViewById(R.id.simpleDatePicker_End); // initiate a date picker
+        simpleDatePicker_End.setSpinnersShown(false);
+
+        simpleTimePicker_Start = (TimePicker)findViewById(R.id.simpleTimePicker_Start); // initiate a time picker
+        simpleTimePicker_Start.setIs24HourView(true);
+        int hours_Start =simpleTimePicker_Start.getCurrentHour();
+        int minutes_Start = simpleTimePicker_Start.getCurrentMinute();
+        System.out.println(hours_Start);
+
+        simpleTimePicker_End = (TimePicker)findViewById(R.id.simpleTimePicker_End); // initiate a time picker
+        simpleTimePicker_End.setIs24HourView(true);
+        int hours_End =simpleTimePicker_End.getCurrentHour();
+        int minutes_End = simpleTimePicker_End.getCurrentMinute();
+        System.out.println(hours_End);
     }
 
     public void onClick(View v) {
