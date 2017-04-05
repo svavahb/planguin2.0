84bde14bf3c1c3dd838e413335802e42a05ef177
thorunn <thorunnarna93@gmail.com>
2017-04-05 18:50:52 +0000

    færa eitt toast

--- app/src/main/java/com/example/svava/planguin/Activities/AddEventActivity.java
+++ app/src/main/java/com/example/svava/planguin/Activities/AddEventActivity.java
@@ -268,6 +268,7 @@ public class AddEventActivity extends AppCompatActivity {
                         Intent intent = new Intent(AddEventActivity.this, ScheduleActivity.class);
                         startActivity(intent);
                         overridePendingTransition(0, 0);
+                        Toast.makeText(AddEventActivity.this, "event created", Toast.LENGTH_SHORT).show();
                     }
                     @Override
                     public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject s) {
@@ -340,7 +341,6 @@ public class AddEventActivity extends AppCompatActivity {
                 //i = new Intent(this, ScheduleActivity.class);
                 stringEventName= myEventName.getText().toString();
                 createScheduleItem(loggedInUser,stringEventName, getStartTime(),getEndTime());
-                Toast.makeText(this, "event created", Toast.LENGTH_SHORT).show();
                 break;
             default:
                 i = new Intent(this, ScheduleActivity.class);
